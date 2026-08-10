8857aa5cd74f49a212f7b570861841fce5945b4e
gtanda <govindstanda@outlook.com>
2026-08-10 06:45:27 -0600

    Setup kanban style project class

---
+++ backend/TaskBoard.Api/Models/Project.cs
@@ -0,0 +1,19 @@
+﻿namespace TaskBoard.Api.Models;
+
+public class Project
+{
+    public Guid Id { get; set; }
+    public string Title { get;  set; }
+    public string? Description { get;  set; }
+
+    public Project (string projectTitle, string? projectDescription)
+    {
+        Id = Guid.NewGuid();
+        Title = projectTitle;
+        Description = projectDescription;
+    }
+
+    public Project()
+    {
+    }
+}
