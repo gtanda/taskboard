1a15482e6b56920cd3fc0d879de75bb2c9139301
gtanda <govindstanda@outlook.com>
2026-08-10 22:22:49 -0600

    Add EF core with postgres and dbcontext

---
+++ backend/TaskBoard.Api/Data/TaskBoardDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskBoard.Api.Models;
+
+namespace TaskBoard.Api.Data;
+
+public class TaskBoardDbContext : DbContext
+{
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<TaskItem> TaskItems { get; set; }
+    
+    public TaskBoardDbContext(DbContextOptions<TaskBoardDbContext> options) : base(options)
+    {
+    }
+    
+    
+}
--- backend/TaskBoard.Api/Program.cs
+++ backend/TaskBoard.Api/Program.cs
@@ -1,9 +1,13 @@
+using TaskBoard.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TaskBoardDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
