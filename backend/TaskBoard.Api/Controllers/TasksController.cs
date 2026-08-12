a1f123e95625d904285cc3348c95aa402be4ad40
gtanda <govindstanda@outlook.com>
2026-08-12 15:26:35 -0600

    Add TaskItemDto to prevent project from leaking into response

--- backend/TaskBoard.Api/Controllers/TasksController.cs
+++ backend/TaskBoard.Api/Controllers/TasksController.cs
@@ -19,22 +19,43 @@ public class TasksController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<TaskItem>>> GetTasksAsync()
+    public async Task<ActionResult<IEnumerable<TaskItemDto>>> GetTasksAsync()
     {
         var tasks = await _context.TaskItems.ToListAsync();
-        return Ok(tasks);
+        var taskDtos = tasks.Select(t => new TaskItemDto
+        {
+            Id = t.Id, 
+            Title = t.Title, 
+            Description = t.Description, 
+            Created =t.Created,
+            State = t.State, 
+            Position = t.Position,
+            ProjectId = t.ProjectId
+        });
+        return Ok(taskDtos);
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<TaskItem>> GetTaskByIdAsync(Guid id)
+    public async Task<ActionResult<TaskItemDto>> GetTaskByIdAsync(Guid id)
     {
         var task = await _context.TaskItems.FindAsync(id);
         if (task is null) return NotFound();
-        return Ok(task);
+        var taskDto = new TaskItemDto
+        {
+            Id = task.Id, 
+            Title = task.Title, 
+            Description = task.Description, 
+            Created = task.Created,
+            State = task.State, 
+            Position = task.Position,
+            ProjectId = task.ProjectId
+        };
+        
+        return Ok(taskDto);
     }
 
     [HttpPost("/api/projects/{projectId}/tasks")]
-    public async Task<ActionResult<TaskItem>> CreateTaskAsync(Guid projectId, CreateTaskDto dto)
+    public async Task<ActionResult<TaskItemDto>> CreateTaskAsync(Guid projectId, CreateTaskDto dto)
     {
         var project = await _context.Projects.FindAsync(projectId);
         if (project is null) return NotFound();
@@ -42,7 +63,17 @@ public class TasksController : ControllerBase
         var newTask = new TaskItem(dto.Title, dto.Description, position, project.Id, project);
         _context.Add(newTask);
         await _context.SaveChangesAsync();
-        return CreatedAtAction("GetTaskById", new {id = newTask.Id}, newTask);
+        var taskDto = new TaskItemDto
+        {
+            Id = newTask.Id, 
+            Title = newTask.Title, 
+            Description = newTask.Description, 
+            Created = newTask.Created,
+            State = newTask.State, 
+            Position = newTask.Position,
+            ProjectId = newTask.ProjectId
+        };
+        return CreatedAtAction("GetTaskById", new {id = newTask.Id}, taskDto);
     }
     
     [HttpDelete("{id}")]
---
+++ backend/TaskBoard.Api/DTOs/TaskItemDto.cs
@@ -0,0 +1,14 @@
+﻿using TaskBoard.Api.Models;
+
+namespace TaskBoard.Api.DTOs;
+
+public class TaskItemDto
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public TaskState State { get; set; }
+    public DateTime Created { get; set; }
+    public int Position  { get; set; }
+    public Guid ProjectId { get; set; }
+}
