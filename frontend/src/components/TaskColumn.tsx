f1c6f6dbad9482369c215c798e2cd5b6ee859ccf
gtanda <govindstanda@outlook.com>
2026-08-12 21:37:14 -0600

    add taskcolumn component, refactor tasklist to be grouped by task status

---
+++ frontend/src/components/TaskColumn.tsx
@@ -0,0 +1,15 @@
+﻿import type {TaskItem} from "../types/taskItem.ts";
+
+interface TaskColumnProps {
+    columnTitle: string;
+    tasks: TaskItem[]
+}
+
+export default function TaskColumn({columnTitle, tasks} : TaskColumnProps) {
+    return <>
+        <h2>{columnTitle}</h2>
+        <ul>
+            {tasks.map(task => <li key={task.id}>{task.title}</li>)}
+        </ul>
+    </>
+}
--- frontend/src/components/TaskList.tsx
+++ frontend/src/components/TaskList.tsx
@@ -1,6 +1,8 @@
 ﻿import {useEffect, useState} from "react";
 import {fetchTasksByProjectId} from "../api/taskItem.ts";
 import type {TaskItem} from "../types/taskItem.ts";
+import {TaskState} from "../types/taskState.ts";
+import TaskColumn from "./TaskColumn.tsx";
 
 interface TaskListProps {
     projectId: string;
@@ -9,6 +11,9 @@ interface TaskListProps {
 export default function TaskList({projectId} : TaskListProps)  {
     const [tasks, setTasks] = useState<TaskItem[]>([]);
     const [error, setError] = useState("");
+    const todoTasks = tasks.filter((t) => t.state === TaskState.Todo);
+    const inProgressTasks = tasks.filter((t) => t.state === TaskState.InProgress);
+    const completedTasks = tasks.filter((t) => t.state === TaskState.Completed);
     
     useEffect(() => {
         const fetchProjectTasks = async () => {
@@ -25,8 +30,8 @@ export default function TaskList({projectId} : TaskListProps)  {
     
     return <>
         {error && <p>{error}</p>}
-        <ul>
-            {tasks.map((t) => <li key ={t.id}>{t.title}</li>)}    
-        </ul>
+            <TaskColumn columnTitle={"To Do"} tasks={todoTasks} />
+            <TaskColumn columnTitle={"In Progress"} tasks={inProgressTasks} />
+            <TaskColumn columnTitle={"Completed"} tasks={completedTasks} />
     </>
 }
