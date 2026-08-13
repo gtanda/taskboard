730ded9913546612b7138c14936cfe5e5dedf257
gtanda <govindstanda@outlook.com>
2026-08-12 20:33:23 -0600

    Add project frontend: types, api client, projectlist component, cors and .env. Removed boilerplate vite conifg too

---
+++ frontend/src/api/project.ts
@@ -0,0 +1,12 @@
+﻿import type {Project} from "../types/project";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export async function fetchProjects(): Promise<Project[]> {
+    const response = await fetch(`${API_BASE_URL}/projects`);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.status}`);
+    }
+    
+    return response.json();
+}
---
+++ frontend/src/components/ProjectList.tsx
@@ -0,0 +1,30 @@
+﻿import {useState, useEffect} from "react";
+import {fetchProjects} from "../api/project";
+import type {Project} from "../types/project.ts";
+
+export default function ProjectList() {
+    const [projects, setProject] = useState<Project[]>([]);
+    const [error, setError] = useState("");
+    
+    useEffect(() => {
+        const fetchProject = async () => {
+            try {
+                const response = await fetchProjects();
+                setProject(response);
+            } catch (err) {
+                setError(err instanceof Error ? err.message : "An unknown error occurred.");
+            }
+        }
+        
+        fetchProject();
+    }, [])
+
+    
+    return <>
+        {error && <p>{error}</p>}
+        <ul>
+            {projects.map((project) => <li key = {project.id}>{project.title}</li>)}
+        </ul>
+    </>
+    
+}
