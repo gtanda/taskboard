93f6a3276552a30ca1f483bf2e7b5b6c5845dcbd
gtanda <govindstanda@outlook.com>
2026-08-13 21:34:16 -0600

    add data validation to DTOs, require titles, length limits, enum and range checks

--- backend/TaskBoard.Api/DTOs/UpdateProjectDto.cs
+++ backend/TaskBoard.Api/DTOs/UpdateProjectDto.cs
@@ -1,7 +1,12 @@
-﻿namespace TaskBoard.Api.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskBoard.Api.DTOs;
 
 public class UpdateProjectDto
 {
+    [Required]
+    [MaxLength(50)]
     public string Title { get; set; }
+    [MaxLength(500)]
     public string? Description { get; set; }
 }
--- backend/TaskBoard.Api/DTOs/UpdateTaskDto.cs
+++ backend/TaskBoard.Api/DTOs/UpdateTaskDto.cs
@@ -1,11 +1,18 @@
-﻿using TaskBoard.Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using TaskBoard.Api.Models;
 
 namespace TaskBoard.Api.DTOs;
 
 public class UpdateTaskDto
 {
+    [Required]
+    [MaxLength(50)]
     public string Title { get; set; }
+    [MaxLength(500)]
     public string? Description { get; set; }
+    [EnumDataType(typeof(TaskState))]
+    [Range(0, int.MaxValue)]
     public TaskState State { get; set; }
+    [Range(0, int.MaxValue)]
     public int Position  { get; set; }
 }
