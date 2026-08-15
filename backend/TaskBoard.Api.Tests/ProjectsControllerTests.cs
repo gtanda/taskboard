934509affdf685fb6953f60dc14f2ae3505f322f
gtanda <govindstanda@outlook.com>
2026-08-14 20:29:48 -0600

    Add integration testing infra, new test db and first integration test

---
+++ backend/TaskBoard.Api.Tests/ProjectsControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+
+namespace TaskBoard.Api.Tests;
+
+public class ProjectsControllerTests : IClassFixture<TaskBoardApiFactory>
+{
+    private readonly HttpClient _client;
+
+    public ProjectsControllerTests(TaskBoardApiFactory factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task PostProject_ReturnsCreate()
+    {
+        var newProject = new { title = "Integration Testing Project", description = "Created by test" };
+
+        var response = await _client.PostAsJsonAsync("/api/projects", newProject);
+        
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+    }
+}
---
+++ backend/TaskBoard.Api.Tests/TaskBoardApiFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TaskBoard.Api.Data;
+
+namespace TaskBoard.Api.Tests;
+
+public class TaskBoardApiFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<TaskBoardDbContext>));
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<TaskBoardDbContext>(options =>
+                options.UseNpgsql(
+                    "Host=localhost;Port=5432;Database=taskboard_test;Username=postgres;Password=devpassword"));
+        });
+    }
+}
